cc5d2c4d85260af4faeaf8c749860fb5bfceaae2
peterjewicz <peterjewicz@totalwebconnections.com>
2020-08-11 11:11:22 -0500

    setup lighting base

---
+++ Assets/Scripts/LightHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightHandler : MonoBehaviour
+{
+    public UnityEngine.Experimental.Rendering.Universal.Light2D PlayerLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      // Debug.Log(PlayerLight.pointLightOuterRadius);
+      PlayerLight.pointLightOuterRadius = PlayerLight.pointLightOuterRadius - 0.001f;
+    }
+}
