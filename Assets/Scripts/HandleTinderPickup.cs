3490ba621d182e770cddb44ccb4132404b9897af
peterjewicz <peterjewicz@totalwebconnections.com>
2020-08-12 14:24:18 -0500

    base stick pickup functionality

---
+++ Assets/Scripts/HandleTinderPickup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleTinderPickup : MonoBehaviour
+{
+
+    public UnityEngine.Experimental.Rendering.Universal.Light2D PlayerLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+      Debug.Log("stick good");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+
+      if(collider.gameObject.name == "Player") {
+        PlayerLight.pointLightOuterRadius = PlayerLight.pointLightOuterRadius += 0.5f;
+        Destroy(gameObject);
+      }
+    }
+}
