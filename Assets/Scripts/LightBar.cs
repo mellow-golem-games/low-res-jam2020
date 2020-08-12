4053d79a55b61267474604a708fbc3f475fc4255
peterjewicz <peterjewicz@totalwebconnections.com>
2020-08-12 13:35:35 -0500

    Add light ui bar that drains

---
+++ Assets/Scripts/LightBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightBar : MonoBehaviour
+{
+    public UnityEngine.Experimental.Rendering.Universal.Light2D PlayerLight;
+    public Slider slider;
+
+    private float startLightVal;
+    private float currentLightVal;
+
+
+    void Start()
+    {
+        startLightVal = PlayerLight.pointLightOuterRadius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentLightVal = PlayerLight.pointLightOuterRadius;
+        float lightRatio = currentLightVal / startLightVal;
+        slider.value = lightRatio;
+    }
+}
