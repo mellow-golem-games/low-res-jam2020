cc8c8d5f4c7d9c95d1229c13edf9460cf5ef38f7
peterjewicz <peterjewicz@totalwebconnections.com>
2020-08-12 21:52:15 -0500

    Enemy follow

--- Assets/Scripts/EnemyHandler.cs
+++ Assets/Scripts/EnemyHandler.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class EnemyHandler : MonoBehaviour
 {
+    public float moveSpeed = 2f;
 
     private Transform targetTransform;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +20,18 @@ public class EnemyHandler : MonoBehaviour
 
     }
 
+    void FixedUpdate() {
+      float step = moveSpeed * Time.deltaTime;
+      Vector2 position = gameObject.transform.position;
+      Vector2 targetPos = targetTransform.position;
+
+      // move sprite towards the target location
+      gameObject.transform.position = Vector2.MoveTowards(transform.position, targetPos, step);
+    }
+
     private void OnTriggerEnter2D(Collider2D collider) {
-      Debug.Log("Handle enemy collision");
+      if (collider.gameObject.name == "Player") {
+        Debug.Log("Handle enemy collision");
+      }
     }
 }
